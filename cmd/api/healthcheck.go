c5527b556b0339ec2039e39ccc86163d316ffa8d
dsolerh <dsolerh.cinter95@gmail.com>
2022-07-13 17:05:14 -0400

    update healthcheck to use errors handlers

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -17,8 +17,6 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
-		app.logger.Println(err)
-		http.Error(w, "The server encountered a problem abd could not process your request", http.StatusInternalServerError)
-		return
+		app.serverErrorResponse(w, r, err)
 	}
 }
