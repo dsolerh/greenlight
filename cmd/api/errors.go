ba4757bdaf3a3e9ea1236ecb896a453342e4c165
dsolerh <dsolerh.cinter95@gmail.com>
2022-07-13 17:04:09 -0400

    add error handlers functions

---
+++ cmd/api/errors.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (app *application) logError(r *http.Request, err error) {
+	app.logger.Println(err)
+}
+
+func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
+	env := envelope{"error": message}
+
+	err := app.writeJSON(w, status, env, nil)
+	if err != nil {
+		app.logError(r, err)
+		w.WriteHeader(500)
+	}
+}
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.logError(r, err)
+
+	app.errorResponse(
+		w,
+		r,
+		http.StatusInternalServerError,
+		"the server encountered a problem and could not process the request",
+	)
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	app.errorResponse(w, r, http.StatusNotFound, "the request resource could not be found")
+}
+
+func (app *application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request) {
+	app.errorResponse(
+		w,
+		r,
+		http.StatusMethodNotAllowed,
+		fmt.Sprintf("the %s method is not supported for this resource", r.Method),
+	)
+}
