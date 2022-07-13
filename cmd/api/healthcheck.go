f4cad4a4af235eb3209e4bc3c0958421a6cd3057
dsolerh <dsolerh.cinter95@gmail.com>
2022-07-13 16:42:51 -0400

    use envelop on healthcheck

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,10 +7,12 @@ import (
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	// js := `{"status":"available","enviroment":%q,"version":%q}`
 	// js = fmt.Sprintf(js, app.config.env, version)
-	data := map[string]string{
-		"status":     "available",
-		"enviroment": app.config.env,
-		"version":    version,
+	data := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"enviroment": app.config.env,
+			"version":    version,
+		},
 	}
 
 	err := app.writeJSON(w, http.StatusOK, data, nil)
